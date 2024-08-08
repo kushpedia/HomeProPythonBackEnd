efdf37f29c283e485d0597c783654f68bb1e9c5c
Henry Wanjiru <haronkuria2015@gmail.com>
2024-08-08 15:27:17 +0300

    initialize

---
+++ homeservicesbackend/profiles/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+import uuid
+from django.contrib.auth.models import User
+# Create your models here.
+class  Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
